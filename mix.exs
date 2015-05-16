c5e22b270f3375364726ff7b206f7d41f252a327
Ramon Snir <r@mon.co.il>
2015-05-16 09:54:52 +0300

    some config updates

--- mix.exs
+++ mix.exs
@@ -1,27 +1,37 @@
 defmodule Eetoul.Mixfile do
-  use Mix.Project
+	use Mix.Project
 
-  def project do
-    [app: :eetoul,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     deps: deps,
+	def project do
+		[app: :eetoul,
+		 version: "0.0.1",
+		 elixir: "~> 1.0",
+		 deps: deps,
 		 default_task: "escript.build",
-		 escript: escript]
-  end
+		 escript: escript,
+		 dialyzer: dialyzer]
+	end
 
 	def application do
 		[applications: [:logger]]
 	end
 
-	def escript do
+	defp escript do
 		[main_module: Eetoul,
 		 path: "bin/eetoul"]
 	end
 
-  defp deps do
+	defp dialyzer do
+		paths = ["eetoul", "geef", "monad"]
+		|> Enum.map(&(File.cwd!() <> "/_build/dev/lib/" <> &1 <> "/ebin"))
+
+		[plt_apps: [:erts, :kernel, :stdlib, :mnesia],
+		 flags: ["-Wunmatched_returns","-Werror_handling","-Wrace_conditions", "-Wno_opaque"],
+		 paths: paths]
+	end
+
+	defp deps do
 		[{:geef, git: "https://github.com/ramonsnir/geef.git"},
 		 {:colorful, "~> 0.6.0"},
 		 {:monad, "~> 1.0.4"}]
-  end
+	end
 end
