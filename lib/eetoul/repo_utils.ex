ed3d5a59de012d4ceca518bd8f91aa76295613ea
Ramon Snir <r@mon.co.il>
2015-07-11 09:59:59 +0300

    maintenance in Eetoul.RepoUtils

--- lib/eetoul/repo_utils.ex
+++ lib/eetoul/repo_utils.ex
@@ -9,7 +9,7 @@ defmodule Eetoul.RepoUtils do
     unless sig do
       sig = Signature.now "Eetoul Test", "test@eetoul"
     end
-    {:ok, tree_id} = make_index repo, files
+    {:ok, tree_id} = write_tree repo, files
     Commit.create repo, sig, sig, message, tree_id, parents
   end
 
@@ -25,17 +25,52 @@ defmodule Eetoul.RepoUtils do
     end
   end
 
-  defp make_index repo, files do
+  defp write_tree repo, files do
     {:ok, odb} = Repository.odb repo
     {:ok, idx} = Index.new
-    {now_mega, now_secs, _} = :os.timestamp
-    time = now_mega * 1000000 + now_secs
     for {path, content} <- files do
-      {:ok, blob_id} = Odb.write odb, content, :blob
-      entry = %Entry{mode: 0o100644, id: blob_id, path: path, size: byte_size(content), mtime: time}
+      entry = write_entry odb, path, content
       :ok = Index.add idx, entry
     end
     Index.write_tree idx, repo
+  end
+
+  defp write_entry odb, path, %{mode: mode, content: content} do
+    {now_mega, now_secs, _} = :os.timestamp
+    time = now_mega * 1_000_000 + now_secs
+    {:ok, blob_id} = Odb.write odb, content, :blob
+    %Entry{mode: mode, id: blob_id, path: path, size: byte_size(content), ctime: time, mtime: time}
+  end
+  defp write_entry odb, path, content do
+    {now_mega, now_secs, _} = :os.timestamp
+    time = now_mega * 1_000_000 + now_secs
+    {:ok, blob_id} = Odb.write odb, content, :blob
+    %Entry{mode: 0o100644, id: blob_id, path: path, size: byte_size(content), ctime: time, mtime: time}
+  end
+
+  defp read_tree repo, tree do
+    {:ok, Enum.into(read_tree(repo, tree, ""), %{})}
+  end
+
+  defp read_tree repo, tree, path do
+    {:ok, odb} = Repository.odb repo
+    files = for %TreeEntry{name: name, mode: mode, type: type, id: id} <- tree do
+      if path != "" do
+        name = "#{path}/#{name}"
+      end
+      case type do
+        :blob ->
+          {:ok, blob} = Blob.lookup repo, id
+          {:ok, content} = Blob.content blob
+          [{name,
+            %{mode: mode,
+              content: content}}]
+        :tree ->
+          {:ok, tree} = Tree.lookup repo, id
+          read_tree repo, tree, name
+      end
+    end
+    Enum.flat_map files, &(&1)
   end
 
   defp resolve_reference _repo, (commit = %Object{type: :commit}) do
